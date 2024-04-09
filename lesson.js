8651229b3b21924be1f748ae8d156008d8360484
neverdal1 <neverdalarild@gmail.com>
2024-04-09 11:51:41 +0200

    unitconverter

---
+++ lesson.js
@@ -0,0 +1,28 @@
+//if else statment
+
+let number = 12; 
+// = erklærer verdi
+// == sjekker innhold
+// === sjekker innhold og datatype
+// < mindre enn / > større enn
+// & og / end
+//  | eller 
+
+
+if (number === 10) {
+    console.log("dette tallet er akkuratt 10")
+}   
+
+else if (number < 10)  {
+    console.log( "dette tallet er mindre eller større en 10")
+}
+
+else if(number > 10){
+    console.log("dette nummeret er større en 10")
+}
+
+else {
+    console.log ("dette talet er IKKE 10")
+
+}
+
--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log('Hello!');
+
+
+
+const submitValue = document.querySelector("#submitValue");
+console.log(submitValue);
+
+submitValue.addEventListener("click", () =>{
+    
+    const inputValue = document.querySelector("#inputValue").value;
+    console.log(inputValue) 
+    
+    const displayValue = document.querySelector("#displayValue");
+console.log(displayValue);
+
+displayValue.textContent = inputValue;
+
+});
+
+
+
+
+
+    
